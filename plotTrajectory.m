cb3b3a9962c5fb7a3f20cfd989c7b9441461ebea
ejpon <36930440+ejpon@users.noreply.github.com>
2025-04-08 21:00:48 +0200

    Create plotTrajectory.m

---
+++ plotTrajectory.m
@@ -0,0 +1,28 @@
+function plotTrajectory(X, t, m1, m2, d)
+%funkce pro vykreslení trajektorie
+%t ... vektor času
+%X ... matice stavů
+
+%chtělo by to ještě poladit velikost plochy, musí být čtverec
+    figure
+    ylim([min(X(:,2)) max(X(:,2))])
+    axis equal
+    for i = 1:length(t)
+        %a - pozice jedné vrtule
+        %b - pozice druhé vrtule
+        a = X(i,1:2)' + m1/(m1+m2)*d*[cos(X(i,3)) ; sin(X(i,3))];
+        b = X(i,1:2)' - m2/(m1+m2)*d*[cos(X(i,3)) ; sin(X(i,3))];
+        
+        
+
+        plot(a(1),a(2),'Color','green',Marker='x',MarkerFaceColor='green',MarkerSize=5)
+        ylim([min(X(:,2)) max(X(:,2))])
+        axis equal
+        ylim([min(X(:,2)) max(X(:,2))])
+        hold on
+        line([a(1) b(1)],[a(2) b(2)],'color','black')
+        plot(b(1),b(2),'Color','blue',Marker='x',MarkerFaceColor='blue',MarkerSize=5)
+        pause(0.1)
+        hold off
+    end
+end
